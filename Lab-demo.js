c9ae20ff4987ae4654bd57d16db2971f1e594643
diane.yu@aquariusuas.com <diane.yu@aquariusuas.com>
2023-06-27 09:37:18 -0400

    fixed order of operation for "app"

--- Lab-demo.js
+++ Lab-demo.js
@@ -1,13 +1,14 @@
 var HTTP_PORT = process.env.PORT || 3000;
 
-const server = app.listen(HTTP_PORT, () => {
-    console.log(`Server running on port ${HTTP_PORT}`);
-  });
 
 
 var express = require("express");
 
 var app = express();
+
+const server = app.listen(HTTP_PORT, () => {
+    console.log(`Server running on port ${HTTP_PORT}`);
+  });
 
 app.get("/", (req, res) => {
     res.send("<html><head></head><body><h1>Hello World!</h1></body></html>");
